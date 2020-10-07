6e5827efd681a9cd8f391d8e9ab293ce225b4edf
Dorival <dorival.querino@gmail.com>
2020-10-07 01:16:46 -0300

    First commit

---
+++ application/app.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "jlearning" {
+  cidr_block = "11.0.0.0/16"
+}
+
+ #   resource "aws_internet_gateway" "jlgw" {
+ #   vpc_id = "${aws_vpc.jlearning.id}"
+#
+ #   tags = {
+ #       Name = "jlearning"
+ #   }
+ #   }
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${aws_vpc.jlearning.id}"
+  cidr_block = "11.0.1.0/24"
+
+  tags = {
+    Name = "jlearning"
+  }
+}
